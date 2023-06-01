7a17a8ef0c60671d2798c9ac71cd1b57e42005c4
JulieKnox225 <julie.knox.cohort225@gmail.com>
2023-06-01 16:38:43 -0700

    updated authentication with cookies

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 // const db = require('./config/pokedexConfig'); //NEEDS TO BE CONNECTED TO ENV
+const cookieParser = require('cookie-parser')
 const cors = require('cors');
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 // app.use(db);
