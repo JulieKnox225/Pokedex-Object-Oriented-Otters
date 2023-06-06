1851138e536f4a8cf702374498e0cc1f5e48b597
Aaron Velasco <avelasco292@gmail.com>
2023-06-06 14:16:16 -0700

    Av's push
    Added a forgot password page. And cleaned up some of the login code.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Login } from './components/Login'
 import { Navbar } from './components/Navbar'
 import { SearchPage } from './components/SearchPage'
 import { Footer } from './components/Footer'
+import { ForgotPassword } from './components/ForgotPassword'
 // import { useState } from 'react'
 
 
@@ -18,6 +19,7 @@ function App() {
     <Route path='/' element ={ <Login />} />
     <Route path='/AddEntryPage' element ={ <AddEntryPage />} />
     <Route path='/SearchPage' element= { <SearchPage /> } />
+    <Route path ='/ForgotPassword' element = {<ForgotPassword/>}/>
   </Routes>
   <Footer />
     </>
