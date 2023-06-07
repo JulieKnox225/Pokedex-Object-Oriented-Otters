31ebb43dbf769da4c950e5cced79072701dcb91e
Andre <andre.rodriguez.cohort229@gmail.com>
2023-06-06 22:46:26 -0700

    Andre's push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,17 @@ import Dropdown from 'react-bootstrap/Dropdown'
 export const Navbar = () => {
   return (
     <div>
-      <div className="container">
-        <label htmlFor="label-check">=</label>
-      </div>
       <nav className='nav-home'>
+      <Dropdown>
+        <Dropdown.Toggle variant='secondary' id="dropdown-button-dark-example1">
+        </Dropdown.Toggle>
+        <Dropdown.Menu variant="dark">
+          <Dropdown.Item href="/">Home</Dropdown.Item>
+          <Dropdown.Item href="/SearchPage">Search Page</Dropdown.Item>
+          <Dropdown.Item href="/AddEntryPage">Add Entry</Dropdown.Item>
+          
+        </Dropdown.Menu>
+      </Dropdown>
         <a href="/" className="home-button">
         <h2>PokeDex</h2>
         </a>
