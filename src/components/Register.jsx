559363878b1daca41e9402845b02db499d00304c
Andre <andre.rodriguez.cohort229@gmail.com>
2023-06-12 13:12:47 -0700

    Andre's Push

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,44 +1,90 @@
-
-
 export const Register = () => {
-
-    return(
-      <div className='register-body'>
+  return (
+    <div className="register-body">
       <form className="register-form">
         <h4 className="sign-up">Sign Up</h4>
-          <div className="form-body">
-              <div className="username">
-                  <label className="form__label" htmlFor="firstName">First Name </label>
-                  <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
-              </div>
-              <div className="lastname">
-                  <label className="form__label" htmlFor="lastName">Last Name </label>
-                  <input className="form__input" type="text" name="" id="lastName" placeholder="Last Name"/>
-              </div>
-              
-              <div className="username">
-                  <label className="form__label" htmlFor="firstName">Username </label>
-                  <input className="form__input" type="text" id="firstName" placeholder="Username"/>
-              </div>
+        <div className="form-body">
+          <div className="username">
+            <label className="form__label" htmlFor="firstName">
+              First Name{" "}
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              id="firstName"
+              placeholder="First Name"
+            />
+          </div>
+          <div className="lastname">
+            <label className="form__label" htmlFor="lastName">
+              Last Name{" "}
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name=""
+              id="lastName"
+              placeholder="Last Name"
+            />
+          </div>
 
-              <div className="email">
-                  <label className="form__label" htmlFor="email">Email </label>
-                  <input className="form__input" type="email" id="email"  placeholder="Email"/>
-              </div>
-              <div className="password">
-                  <label className="form__label" htmlFor="password">Password </label>
-                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
-              </div>
-              <div className="confirm-password">
-                  <label className="form__label" htmlFor="confirmPassword">Confirm Password </label>
-                  <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
-                    <p className="last-sentence">Password must be at least 12 characters long. Weak password will be marked as invalid. Password must not be greater than 128 characters long.</p>
-              </div>
+          <div className="username">
+            <label className="form__label" htmlFor="firstName">
+              Username{" "}
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              id="firstName"
+              placeholder="Username"
+            />
           </div>
-          <div className="submit-register">
-              <button type="submit" className="create-acc-btn">Create New Account</button>
+
+          <div className="email">
+            <label className="form__label" htmlFor="email">
+              Email{" "}
+            </label>
+            <input
+              className="form__input"
+              type="email"
+              id="email"
+              placeholder="Email"
+            />
           </div>
-      </form>   
-    </div>  
-    )       
-}
+          <div className="password">
+            <label className="form__label" htmlFor="password">
+              Password{" "}
+            </label>
+            <input
+              className="form__input"
+              type="password"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="confirm-password">
+            <label className="form__label" htmlFor="confirmPassword">
+              Confirm Password{" "}
+            </label>
+            <input
+              className="form__input"
+              type="password"
+              id="confirmPassword"
+              placeholder="Confirm Password"
+            />
+            <p className="last-sentence">
+              Password must be at least 12 characters long. Weak password will
+              be marked as invalid. Password must not be greater than 128
+              characters long.
+            </p>
+          </div>
+        </div>
+        <div className="submit-register">
+          <button type="submit" className="create-acc-btn">
+            Create New Account
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
