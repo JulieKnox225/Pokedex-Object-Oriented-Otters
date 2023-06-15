8ff90795117158bfee5275e50a0d532c53ff1920
JulieKnox225 <julie.knox.cohort225@gmail.com>
2023-06-14 18:12:38 -0700

    search logic

--- server/controllers/pokedexControllers.js
+++ server/controllers/pokedexControllers.js
@@ -9,6 +9,68 @@ const getAllPokemon = async (req, res) => {
     }
 };
 
+/** 
+ * This should be able to replace all other getPokemonBy____ 
+
+        const { search, type } = req.query;
+        let result = [];
+        if(search) {
+            switch (type) {
+                case 'name':
+                    result = await req.db.query(`SELECT * FROM plants WHERE name LIKE '%${search}%'`);
+                    break;
+                case 'nickname':
+                    result = await req.db.query(`SELECT * FROM plants WHERE nickname LIKE '%${search}%'`);
+                    break;
+                case 'months_to_plant':
+                    result = await req.db.query(`SELECT * FROM plants WHERE months_to_plant LIKE '%${search}%'`);
+                    break;
+                case 'sun_req':
+                    result = await req.db.query(`SELECT * FROM plants WHERE sun_req = :search`, { search });
+                    break;
+                case 'planting_zone':
+                    result = await req.db.query(`SELECT * FROM plants WHERE planting_zone = :search`, { search });
+                    break;
+                case 'sow_temp_range':
+                    result = await req.db.query(`SELECT * FROM plants WHERE sow_temp_range LIKE '%${search}%'`);
+                    break;
+                case 'fertilizer_NPK':
+                    result = await req.db.query(`SELECT * FROM plants WHERE fertilizer_NPK = :search`, { search });
+                    break;
+                case 'description':
+                    result = await req.db.query(`SELECT * FROM plants WHERE description LIKE '%${search}%'`);
+                    break;
+                default:
+                    result = await req.db.query(`SELECT * FROM plants WHERE :type = :search`, { type, search });
+                    break;
+            }
+        }
+        else {
+            result = await req.db.query(`SELECT * FROM plants`);
+        }
+
+        if(result[0].length === 0) {
+            result[0].push({ name: 'Sorry none found!' });
+        }
+
+        res.status(200).json(result[0]);
+        
+ * Ideally, I wanted to do something similar to the following. However, type is sent to MySQL with '' around the word
+    which causes errors :/
+        const wordsToSearchWithLike = ['name', etc]
+        ...
+        case 'name':
+            result = await req.db.query(`SELECT * FROM plants WHERE :type LIKE '%${search}%'`, { type });
+            break;
+        default:
+            result = await req.db.query(`SELECT * FROM plants WHERE :type = :search`, { type, search });
+            break;
+        
+
+*/
+
+
+
 const getPokemonById = async (req, res) => {
     try {
         const { id } = req.params;
